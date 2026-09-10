99633194959e0b1811699a7cad9ac884c5dfbf1b
Amir-FF <amir.computer.04.ir@gmail.com>
2026-09-10 18:06:34 +0330

    add inheritance

--- app.js
+++ app.js
@@ -5,10 +5,22 @@ class Person {
   }
 
   gitInfo() {
-    return `my name is ${this.name} && ${this.age}`;
+    return `my name is ${this.name} && ${this.age} && color ${this.color}`;
+  }
+}
+
+class Student extends Person {
+  constructor(name, age, color) {
+    super(name, age);
+    this.color = color;
+  }
+
+  sleep() {
+    return "I am sleeping now!";
   }
 }
 
 const person = new Person("Amir.FF", 22);
+const student = new Student("Amir.FFE", 17, "red");
 
-console.log(person.gitInfo());
+console.log(student.gitInfo());
